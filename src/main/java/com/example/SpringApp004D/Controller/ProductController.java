b6656268923b60075c145dc81cf087020a623761
j.riquelmee <j.riquelmee@profesor.duoc.cl>
2025-05-28 10:29:36 -0400

    Unidad 3.2 Implementacion de librerias OpenApi y Swagger , configuracion inicial de proyecto y servicios aplicados

--- src/main/java/com/example/SpringApp004D/Controller/ProductController.java
+++ src/main/java/com/example/SpringApp004D/Controller/ProductController.java
@@ -2,22 +2,27 @@ package com.example.SpringApp004D.Controller;
 
 import com.example.SpringApp004D.Model.Product;
 import com.example.SpringApp004D.Service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name="Productos",description = "Servicios de gestion de productos Fullstack I")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @GetMapping
+    @Operation(summary = "Obtener productos",description = "Servicio GET para obtener la lista completa de productos existentes")
     public String getAllProducts(){
         return productService.getAllProducts();
     }
 
     @PostMapping
+    @Operation(summary = "Agregar Producto",description = "Servicio POST para agregar un nuevo producto en el sistema")
     public String addProduct(@RequestBody Product product){
         return productService.addProduct(product);
     }
